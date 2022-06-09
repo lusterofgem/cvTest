a9b7e7ff6d1ed9c4c285bbae51089e0e0e9ab921
kde <lusterofgem@gmail.com>
2022-06-09 21:52:59 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+
+int main()
+{
+    std::filesystem::path imagePath = "background.jpeg";
+
+    cv::Mat image = cv::imread(imagePath.filename());
+
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3,3));
+    cv::Mat destImg;
+    cv::morphologyEx(image, destImg, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 10);
+
+    cv::imwrite(imagePath.stem().string() + "_close" + imagePath.extension().string(), destImg);
+    return 0;
+}
